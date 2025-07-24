932db2f614dc7830a2313236793297611e9678a0
hav0kinho <ruallysonfelype@gmail.com>
2025-07-23 22:19:11 -0300

    Configuração do INSTALLED_APPS e da Autenticação JWT do Rest

--- catalogapp/settings.py
+++ catalogapp/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'rest_framework_simplejwt',
+
+    'products',
+    'categories'
 ]
 
 MIDDLEWARE = [
@@ -120,3 +126,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# CONFIGURAÇÕES ADICIONAIS
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ),
+}
+

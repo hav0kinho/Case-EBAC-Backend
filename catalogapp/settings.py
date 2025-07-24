abefce07dd84adbe28695208b23c99fd5dd90832
hav0kinho <ruallysonfelype@gmail.com>
2025-07-24 15:04:08 -0300

    Erro Resolvido: Problema com o CORS que não estava comunicando corretamente com o Frontend

--- catalogapp/settings.py
+++ catalogapp/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'rest_framework_simplejwt',
+    'corsheaders',
 
     'products',
     'categories',
@@ -50,6 +51,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -130,6 +132,12 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # CONFIGURAÇÕES ADICIONAIS
+
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:5173",  # ou a porta do seu front-end
+# ]
+
+CORS_ALLOW_ALL_ORIGINS = True # So se tiver dando pau
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (

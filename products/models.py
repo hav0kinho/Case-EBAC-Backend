186f29299c8e98db0a83de522d2698c2cd70bf55
hav0kinho <ruallysonfelype@gmail.com>
2025-07-23 22:25:50 -0300

    Criação dos modelos inicias e seus relacionamentos

--- products/models.py
+++ products/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from categories.models import Category
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    image_url = models.URLField(blank=True) # Não sei se vou usar
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField(default=0)
+    active = models.BooleanField(default=True)
+
+    
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products') # Relacionamento com o modelo de categorias
+
+    # Para desencargo de consciência novamente
+    def __str__(self):
+        return self.name

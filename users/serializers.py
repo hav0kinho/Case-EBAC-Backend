5c86f581f229912551fe504917992fddaeb0cf79
hav0kinho <ruallysonfelype@gmail.com>
2025-07-25 03:25:59 -0300

    Melhoria no sistema de autenticação do Usuário

--- users/serializers.py
+++ users/serializers.py
@@ -1,17 +1,22 @@
-from django.contrib.auth.models import User
+
 from rest_framework import serializers
+from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
 
 class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'password']
+        fields = ['username', 'email', 'password', 'password2']
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "As senhas não coincidem."})
+        return attrs
 
     def create(self, validated_data):
-        user = User.objects.create_user(
-            username=validated_data['username'],
-            email=validated_data.get('email', ''),
-            password=validated_data['password']
-        )
+        validated_data.pop('password2')
+        user = User.objects.create_user(**validated_data)
         return user
--- users/views.py
+++ users/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 
-from rest_framework import generics
+from rest_framework import generics, permissions
 from django.contrib.auth.models import User
 from .serializers import UserSerializer
 
 class UserRegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [permissions.AllowAny]
